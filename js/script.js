a4f5c3f7ea2e8f79e607e3e57db69492b5ab0544
Manikandan B <118624712+Manikandansns@users.noreply.github.com>
2024-07-10 17:07:31 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Frontend Developer","Content Writer"],
+    strings:["","Frontend Developer","Web Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
